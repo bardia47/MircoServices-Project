0a34c83d4a37ed457e3c2e2d53a9ca0f45e8c77a
bardia47 <bardiadorry@yahoo.com>
2023-12-29 14:01:44 +0330

    add resilience4j retry

--- PhotoAppApiUsers/src/main/java/com/microservices/photoappapiusers/service/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/microservices/photoappapiusers/service/AlbumsServiceClient.java
@@ -2,6 +2,7 @@ package com.microservices.photoappapiusers.service;
 
 import com.microservices.photoappapiusers.model.AlbumResponseModel;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +14,10 @@ import java.util.List;
 public interface AlbumsServiceClient {
     @GetMapping("users/{id}/albums")
     @CircuitBreaker(name = "albums-ws", fallbackMethod = "getAlbumsFallback")
+    @Retry(name = "albums-ws")
     List<AlbumResponseModel> getAlbums(@PathVariable("id") String id);
 
-    default List<AlbumResponseModel> getAlbumsFallback(String id, Throwable x) {
+    default  List<AlbumResponseModel> getAlbumsFallback(String id, Throwable x) {
         System.out.println("Param :" + id);
         System.out.println("Exception took place" + x.getMessage());
         return new ArrayList<>();
